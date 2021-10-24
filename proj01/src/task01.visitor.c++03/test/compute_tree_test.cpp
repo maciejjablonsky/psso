4304da34cfddc248b498d6dfb53d2d9c330e7704
maciejjablonsky <maciekjablonsky@gmail.com>
2021-10-24 17:42:19 +0200

    Added prefix visitor c++03 version
    Signed-off-by: maciejjablonsky <maciekjablonsky@gmail.com>

--- proj01/src/task01.visitor.c++03/test/compute_tree_test.cpp
+++ proj01/src/task01.visitor.c++03/test/compute_tree_test.cpp
@@ -3,6 +3,7 @@
 #include <task01.visitor.c++03/infix_print_visitor.hpp>
 #include <task01.visitor.c++03/parser.hpp>
 #include <task01.visitor.c++03/posfix_print_visitor.hpp>
+#include <task01.visitor.c++03/prefix_print_visitor.hpp>
 
 TEST(ComputeTreeTest, InfixPrintVisitor)
 {
@@ -32,4 +33,14 @@ TEST(ComputeTreeTest, PostfixPrintVisitor)
     root_node->accept(visitor);
 
     ASSERT_EQ(visitor.report(), "1 2 3 4 * - 5 / +");
+}
+
+TEST(ComputeTreeTest, PrefixPrintVisitor)
+{
+    psso::Parser parser;
+    auto root_node = parser.parse("1 + ( 2 - 3 * 4 ) / 5");
+    psso::PrefixPrintVisitor visitor;
+    root_node->accept(visitor);
+
+    ASSERT_EQ(visitor.report(), "+ 1 / - 2 * 3 4 5");
 }
